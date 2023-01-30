c051f7e9405e2f0a569810948633991aedb5a6a2
mohamed elmhassani <mohamed_elmhassani@live.fr>
2023-01-30 02:42:52 +0100

    Update git ignore

--- src/api/movies/getMovie.jsx
+++ src/api/movies/getMovie.jsx
@@ -1,12 +1,8 @@
 import { AxiosInstance } from "../AxiosInstance";
 
-const sleep = (duration) =>
-  new Promise((res) => setTimeout(res, duration * 1000));
-
 export const getMovie = async (id) => {
   try {
     const { data } = await AxiosInstance.get(`/movies/${id}?populate=*`);
-    await sleep(5);
     return data;
   } catch {
     throw new Response("Title Not Found", { status: 404 });
